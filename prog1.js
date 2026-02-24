57d53a914bc4ad2b65e843ce1470d9d024e32e82
Muzammil Hussain <muzammil.org73@gmail.com>
2026-02-24 23:05:17 +0500

    Add files via upload

---
+++ prog1.js
@@ -0,0 +1,14 @@
+console.log("hello muzammil");
+const acountId=1443;
+console.log(acountId);
+let acountEmail="mhe9404934039";
+var acountName="muzammil hussain";
+var acountpassword="2389489238";
+acountCity="lahore";
+console.table([acountEmail, acountId, acountName, acountpassword, acountCity]);
+ 
+let name="ali"; //string type
+let age= 22 ;   // int type
+ let  num= true ;   // bool type
+
+
