0a4b1747b60e335494230260b138f967e2329c7b
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-04 21:52:41 +0500

    Add files via upload

---
+++ prog6.js
@@ -0,0 +1,20 @@
+let score=true;
+let num=Number(score);
+console.log(score);
+console.log(num);
+let number=1;
+console.log(number);
+let bool=Boolean(number);
+console.log(bool);
+// 1==true
+// 0= fals
+//" "=fals
+//"ali"=true
+let string="";
+let string2="ali";
+console.log(string);
+console.log(string2);
+let string3=Boolean(string);
+let string4=Boolean(string2);
+console.log(string3);
+console.log(string4);
