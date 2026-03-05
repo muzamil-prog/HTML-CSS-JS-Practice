e1c264e6a6db886aa555569a69c2406a4bf21ab7
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-05 22:35:19 +0500

    Add files via upload

---
+++ prog7.js
@@ -0,0 +1,36 @@
+let num=33;
+console.log(typeof(num));
+let string=String(num);
+console.log(typeof(string));
+// aply operation on data====
+
+let value=3;
+console.log(value);
+let nevalue=-value;
+console.log(nevalue);
+//=====================
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%2);
+
+///===== aply addtion operation on string dat======
+let str1="ali";
+let stri="ahmad";
+console.log(str1+stri);
+console.log(stri-str1);// this operation is not aply on string data
+
+//===============
+
+console.log("1"+2);// 12
+console.log(1+"2");// 12
+console.log(1+2+"2");// 32
+console.log(+true);// 1
+console.log(true);// true
+let game=100;
+++game;//increament the value
+console.log(game);// 101
