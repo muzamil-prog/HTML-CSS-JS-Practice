7fe2ef8e5040562b043a4062ebe9292da1fc9a48
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-05 22:36:00 +0500

    Add files via upload

---
+++ prog8.js
@@ -0,0 +1,12 @@
+// non primitve data types
+const score=100; //number
+const score1=100.3;// float data type
+const score2= true; // boolean
+const score3 ="123"; // string 
+const id=symbal('1,2,3'); // symbal type used for uniqure
+// for example we chec symbal is unique lets try and check
+const anotherId=symbal('1,2,3');
+// lets compaire and check unique
+console.log(id==anotherId);
+
+
