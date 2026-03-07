8dee69344780d8b7a0aec6d893ceb161670dc3c8
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-07 23:41:44 +0500

    Add files via upload

---
+++ prog10.js
@@ -0,0 +1,19 @@
+// non primitative data typees
+// 1===== array====
+// const hero={"ali", "muzamiil", "ali hasan","gulam rasol",};
+// console.log(hero);
+
+// 2=== objects===
+
+let myobject={
+    name: "ali",
+    age: 22,
+}
+console.log(myobject);
+n
+
+
+// 3=== functions=====
+const myFunction= myFunction(){
+    console.log("hello world");
+}
