edd860b991f420c6dba3781b9de95785ead96c84
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-07 23:53:08 +0500

    Add files via upload

---
+++ prog11.js
@@ -0,0 +1,22 @@
+console.log("condition statements");
+let age=25;
+if(age>18){
+    console.log("you can drive the car");
+}else {
+    console.log("you can not drive the car");
+}
+
+
+
+a=10;
+b=8;
+ let ans=a>b?(a-b):(b-a); // modern method
+console.log(ans)
+//alter or old methode
+if(a>b){
+    console.log(a-b);
+}else{
+    console.log(b-a);
+}
+
+
