4a4211c4bbdcad28f92d3d1766079bf4b85ba780
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-10 22:15:41 +0500

    Add files via upload

---
+++ prog17.js
@@ -0,0 +1,11 @@
+console.log("this is a function");
+// a block of code that can be used for reapeatly
+function nice(name){
+    console.log("hey  "+name+ "  your nice man");
+
+
+}
+nice("muzammil");
+nice("qaim");
+nice("ali");
+
