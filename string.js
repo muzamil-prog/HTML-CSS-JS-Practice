a51c38e71506107fe52b4d1e304f0cfeee131e69
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-12 21:18:54 +0500

    Add files via upload

---
+++ string.js
@@ -0,0 +1,17 @@
+console.log("this is   string topic")
+let name="muazzail";
+console.log(name)
+console.log(name.length);
+console.log(name[0]);
+
+ let real_name="Muzammil Hussain";
+let friend="umer shehzaid";
+// console.log('his name is ${real_name} and his friend is '$(friend));
+
+console.log("his name is:" +name+"and his friend is:"+friend);
+ console.log(friend.toUpperCase());
+console.log(`his name is ${real_name} and hsi friend name is ${friend}`)
+console.log(real_name.toLowerCase())
+console.log(real_name.slice(1,4));
+console.log(real_name.slice(4));
+console.log(real_name.slice(1,14));
