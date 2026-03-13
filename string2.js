70a77e71cf73d7ce21022816087b504bf2e54425
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-13 23:56:02 +0500

    Add files via upload

---
+++ string2.js
@@ -0,0 +1,11 @@
+console.log("this is a string ")
+// repalce method
+let name="muzammil hussain";
+ console.log(name.replace("mu","77"));
+console.log(name.replace("muz", "333", "muxammil"))
+// concate method
+let a="ali";
+let b="muzaammil";
+console.log(a.concat(b));
+console.log(a.concate(b),"ali hamza ")``
+let c=34;
