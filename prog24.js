8a2225a245632c3772c5773fbac5690f0f1c99c9
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-27 22:16:14 +0500

    Add files via upload

---
+++ prog24.js
@@ -0,0 +1,12 @@
+console.log('rates of tour in pakistan')
+var maree= 50000;
+var hunza=40000;
+var nargan=60000;
+var familMember=6;
+if(familMember==6){
+    var desount=maree*0.20;
+    maree=maree-desount;
+
+    console.log('after descount the tour rate' +maree)
+}else{
+    console.log('no descount');}
