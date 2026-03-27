8a12ee88d324d43e059dccbcf7f6ff20bcceb626
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-27 22:16:50 +0500

    Add files via upload

---
+++ prog25.js
@@ -0,0 +1,13 @@
+// for(let x=1; x<=50; x++){
+//     if(x%2==0){
+//         console.log('this is even number')
+//         console.log(x )
+//     }else{
+//         console.log('odd number')
+//         console.log(x)
+//     }
+// }
+var num=30;
+for(let x=1; x<=50; x++){
+    console.log(num, "*", x, "=", x*num )
+}
