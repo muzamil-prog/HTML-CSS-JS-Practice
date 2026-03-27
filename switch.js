9dd0a6b6b2655394fad64e548b17a749c7ddb79d
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-27 22:17:40 +0500

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,25 @@
+//  switch statement
+// break kr skty if given number are found
+// if else statement sy fast hota ha
+var num=5;
+switch (num) {
+    case 1:
+        console.log('a')
+        
+        break;
+        case 2:
+            console.log('b');
+  break;
+  case 3:
+    console.log('c')
+    break;
+    case 4:
+        console.log('d')
+        break;
+        case 5:
+            console.log('e')
+            break;
+    default:
+        console.log('invalid number please try again')
+        break;
+}
