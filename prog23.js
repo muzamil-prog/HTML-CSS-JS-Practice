d4554dbc7ff7f682b4bbf697d469721e37f61c9f
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-27 22:15:31 +0500

    Add files via upload

---
+++ prog23.js
@@ -0,0 +1,17 @@
+// menu
+console.log('........ menu...........')
+var menu=1;
+if(menu===1){
+    console.log('chose burger price is 350');
+}else if(menu==2){
+    console.log('you chose a piza');
+    console.log('price is 360');
+}
+else if(menu==3){
+    console.log('you chose shurama')
+} else if(menu==5){
+    console.log('you chose gold drink');
+    console.log('price is 400');
+}else{
+    console.log('invalid statement');
+}
