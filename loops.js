4ddbb2af162e7f88f881170679914068d96e6b5e
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-28 23:17:27 +0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,9 @@
+// loops
+//----for loops-----
+// for loops ma ending point deya hota ha yani condtion di hoti ha ky yha tk hmm ny print krna ha 
+for(let x=0; x<=5; x++){
+    console.log(x)
+}
+for(let x=1; x<=50; x++){
+    console.log('pakistan')
+}
