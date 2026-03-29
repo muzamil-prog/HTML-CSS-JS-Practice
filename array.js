1263e37b49e31644868903931bee40d6e83aea66
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-29 21:44:43 +0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,12 @@
+// var arr=[12,23,45,56]
+// console.log(arr.length)
+// for(var i=0; i<arr.length; i++){
+//     console.log(arr[i]+5);
+// }
+for(var x=1; x<5; x++){
+    console.log(x)
+    for(let i=0; i<3; i++){
+        console.log('pakistan zindabad');
+    }
+    console.log('---------------');
+}
