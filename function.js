fabdb2ef02def355d61bc7fcc640ebc9bc16b125
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-31 23:14:16 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,14 @@
+// functions
+function success(){
+    console.log("login Succcessfully")
+}
+function falire(){
+    console.log("incorrect Id and passsword ");
+}
+var user="mh3659923";
+if(user=="mh3659923"){
+    success()
+}else{
+    falire()
+}
+// xamp     
