40674bba4023b54411f4383ea6c0bc4f58991c30
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-31 23:15:14 +0500

    Add files via upload

---
+++ functionprog2.js
@@ -0,0 +1,10 @@
+// check even or odd
+var n=19;
+function check(){
+    if(n%2==0){
+        console.log("the number is even")
+    }else{
+        console.log("the number is odd")
+    }
+}
+check();
