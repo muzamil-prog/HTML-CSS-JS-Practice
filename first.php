6f42980918eb88ff132b4acac905512047272ef8
Muzammil Hussain <muzammil.org73@gmail.com>
2026-04-07 23:19:39 +0500

    Add files via upload

---
+++ first.php
@@ -0,0 +1,29 @@
+<?php
+
+echo"hello php \n";
+$x=1; // string data type
+echo $x;
+echo"<br>";
+echo"<h2> hello world<h2>";
+$b=2;
+echo $b;
+
+    $r=20;
+    $u=30;
+    echo"<br>";
+    echo'the value of r is <br>'.$r;
+    echo"the value of u is".$u;
+      $result=$r+$u;
+      echo"<br>";
+      echo"the result of {$r} and {$u} <br>".$result;
+       $result=$r-$u;
+      echo"<br>";
+      echo"the result of {$r} and {$u} <br>".$result;
+       $result=$r/$u;
+      echo"<br>";
+      echo"the result of {$r} and {$u} <br>".$result;
+       $result=$r*$u;
+      echo"<br>";
+      echo"the result of {$r} and {$u} <br>".$result;
+
+?>
