cb17caff7bb86f4217321e1dc006c49929d39224
Muzammil Hussain <muzammil.org73@gmail.com>
2026-04-17 23:37:13 +0500

    Add files via upload

--- home.php
+++ home.php
@@ -1,1 +1,34 @@
-<h1> This is home page </h1>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+   <img src="darazlogoo.png"  class="img-fluid" alt="" height="50px" width="100px">
+    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarContent">
+
+     
+
+      <form class="d-flex search-box mx-auto w-50">
+        <input class="form-control me-2" type="search" placeholder="Search">
+        <button class="search-btn">Search</button>
+      </form>
+
+    </div>
+  </div>
+</nav>
+
+    
+    <h1>welcome about page</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
