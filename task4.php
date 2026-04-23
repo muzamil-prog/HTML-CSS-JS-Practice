f0e9c7b03d76d407f9188bdfd054a5b1d172aa5a
Muzammil Hussain <muzammil.org73@gmail.com>
2026-04-23 23:23:07 +0500

    Add files via upload

---
+++ task4.php
@@ -0,0 +1,42 @@
+<?php
+require 'db.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1 class="bg-info text-center ">Student Management System</h1>
+    <form class="bg-info p-4 w-50 m-auto" method="POST"  action="code.php>
+
+    <div class="mb-3">
+      <label class="form-label">Name</label>
+      <input type="text" class="form-control" name="name" >
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">Course</label>
+      <input type="text" class="form-control" name="course">
+    </div>
+    
+    <div class="mb-3">
+      <label class="form-label">Fee</label>
+      <input type="text" class="form-control" name="fees" >
+    </div>
+    
+    <div class="mb-3">
+      <label class="form-label">City</label>
+      <input type="text" class="form-control" name="city" >
+     </div>
+  <button name="reguser" type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
