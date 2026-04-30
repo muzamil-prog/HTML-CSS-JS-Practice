03fc99b4ae1b39a0244b36c11d1b757e141f1e6e
Muzammil Hussain <muzammil.org73@gmail.com>
2026-04-30 23:37:14 +0500

    Add files via upload

---
+++ araay4.js
@@ -0,0 +1,17 @@
+// array
+
+let num =[1,2,3,4,5];
+console.log(num);
+console.log(num[2], 4);
+console.log(num,4);
+num[2]=6;
+console.log(num);
+
+for(let index=0; index<=num.length; index++){
+    num=(num[index+1])
+    console.log(num);
+}
+
+
+
+let array=[1,3,4,4,3];
