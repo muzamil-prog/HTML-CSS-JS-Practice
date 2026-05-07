f0da9c41006c3fab1eb10de67d8778c757175e2c
Muzammil Hussain <muzammil.org73@gmail.com>
2026-05-08 00:03:00 +0500

    Add files via upload

---
+++ compa.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const compa = () => {
+  return (
+    <div>
+        <h1> username is {props.name}</h1>
+         <h1> username is {props.age}</h1>
+        
+    </div>
+  )
+}
+
+export default compa
